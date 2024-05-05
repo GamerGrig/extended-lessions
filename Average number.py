cc9717b29b88dc4d6860ce1fec795d0bb2834c56
GamerGrig <59883301+GamerGrig@users.noreply.github.com>
2024-05-05 13:02:30 +0500

    Add files via upload

---
+++ Average number.py
@@ -0,0 +1,10 @@
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+c = int(input('Введите третье число: '))
+
+if a <= b <= c or c <= b <= a:
+    print(b)
+elif b <= a <= c or c <= a <= b:
+    print(a)
+else:
+    print(c)
